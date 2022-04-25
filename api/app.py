19f2a7c2671e7cda3a975eafeabba954fe611639
Chris <christianyeganeh@gmail.com>
2022-04-24 20:06:46 -0400

    Added the ability to register a user by an API endpoint. Added verification to registration parameters and changed the default Admin Password because of this.

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 from tools import *
 from flask import Flask, request
 from flask_cors import CORS
-
+from flask.app import BadRequest
 
 app = Flask(__name__)
 CORS(app)
@@ -33,8 +33,36 @@ def api_system_setup():
 
 @app.route('/auth/register', methods=['POST'])
 def api_auth_register():
+    user_json = None
+    try:
+        user_json = request.json
+        for req in ['username', 'first_name', 'last_name', 'email', 'password', 'password_confirm']:
+            if req not in user_json:
+                raise SyntaxError(f'Missing {req} in user json.')
+        user_username = user_json['username']
+        user_first_name = user_json['first_name']
+        user_last_name = user_json['last_name']
+        user_email = user_json['email']
+        user_password = user_json['password']
+        user_password_confirm = user_json['password_confirm']
 
-    return {}
+        if user_password != user_password_confirm:
+            raise ValueError('Make sure the passwords match.')
+
+        result, exc = register_user(user_username, user_first_name, user_last_name, user_email, user_password)
+        if not result:
+            raise exc
+        else:
+            return make_api_response('Successfully created user.', data=user_json, new_token='abc')
+    except BadRequest as e:
+        return make_api_response('There was an error creating the user: '
+                                 'Make sure to include a json object with the required fields.', status=400)
+    except SyntaxError as e:
+        return make_api_response('There was an error creating the user: ' + str(e), status=400)
+    except ValueError as e:
+        return make_api_response('There was an error creating the user: ' + str(e), status=400)
+    except Exception as e:
+        return make_api_response('There was an error creating the user: ' + repr(e), status=400)
 
 
 
--- api/tools.py
+++ api/tools.py
@@ -3,6 +3,7 @@ from constants import *
 import psycopg
 import hashlib
 import secrets
+import re
 
 def make_api_response(msg='', data=None, status=200, new_token=None):
     return make_response({
@@ -14,6 +15,17 @@ def make_api_response(msg='', data=None, status=200, new_token=None):
 
 def register_user(username, first_name, last_name, email, password, roles=[ROLE_USER]):
     try:
+        if not check_username(username):
+            raise ValueError('Make sure the username matches the required formatting.')
+        if not check_name(first_name):
+            raise ValueError('Make sure the first name matches the required formatting.')
+        if not check_name(last_name):
+            raise ValueError('Make sure the last name matches the required formatting.')
+        if not check_email(email):
+            raise ValueError('Make sure the email matches the required formatting.')
+        if not check_password(password):
+            raise ValueError('Make sure the password matches the required formatting.')
+
         with psycopg.connect(
             host = POSTGRES_HOST,
             user=POSTGRES_USER,
@@ -24,6 +36,8 @@ def register_user(username, first_name, last_name, email, password, roles=[ROLE_
                          '(%s, %s, %s, %s, %s, %s)',
                          (username, first_name, last_name, email, hashed_password, salt))
         return True, None
+    except psycopg.errors.UniqueViolation as e:
+        raise ValueError('That username already exists in the system.')
     except Exception as e:
         return False, e
 
@@ -37,3 +51,16 @@ def salt_password(password, salt=None):
         100000
     ).hex()
     return hashed_password, salt
+
+def check_username(username):
+    return re.fullmatch(r'[a-zA-Z0-9\-\_]{4,32}', username) is not None
+def check_name(name):
+    return re.fullmatch(r'[a-zA-Z0-9\-]{0,32}', name) is not None
+def check_email(email):
+    return re.fullmatch(r'[a-zA-Z\.\-]+@([a-zA-Z\-]+\.)+[a-zA-Z\-]+', email) is not None and len(email) <= 64
+def check_password(password):
+    pattern_count = 0
+    for pattern in [r'[a-z]', r'[A-Z]', r'[0-9]', r'[!@#$%^&*()_\-+=\[\]{}:;\"\'<>,\.?/\\|`~]']:
+        if re.search(pattern, password):
+            pattern_count += 1
+    return pattern_count >= 3 and (8 <= len(password) <= 128)
